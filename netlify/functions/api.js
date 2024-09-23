9799b437d74966c4dee336f2c91f376b6d450df2
coopdloop <coopdevsec@proton.me>
2024-09-23 12:30:42 -0500

    chore: remove eslint for standard

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,21 +1,21 @@
 // api.js
 
-async (event) => {
+exports.handler = async (event) => {
   if (event.httpMethod === 'GET') {
     try {
       // Process the GET request as needed
-      const data = require('./db.json');
+      const data = require('./db.json')
 
       // Return the data as the response
       return {
         statusCode: 200,
-        body: JSON.stringify(data),
-      };
+        body: JSON.stringify(data)
+      }
     } catch (error) {
       return {
         statusCode: 500,
-        body: JSON.stringify({ error: `${error}` }),
-      };
+        body: JSON.stringify({ error: `${error}` })
+      }
     }
   }
-};
+}
